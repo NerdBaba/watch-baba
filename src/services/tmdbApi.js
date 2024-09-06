472b9d8473ca73edda446ee7048713f7b1130cd4
Baba <76611905+NerdBaba@users.noreply.github.com>
2024-09-07 02:41:55 +0530

    Update tmdbApi.js

--- src/services/tmdbApi.js
+++ src/services/tmdbApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://simple-proxy.mda2233.workers.dev/?destination=https://api.themoviedb.org/3';
 
 const tmdbApi = axios.create({
   baseURL: BASE_URL,
